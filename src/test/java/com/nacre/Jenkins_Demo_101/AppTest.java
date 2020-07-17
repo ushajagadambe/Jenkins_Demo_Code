2ed46211c14629ff7ab2cfe876b4437c9b85d7d4
ushajagadambe <jagadambeusha@gmail.com>
2020-07-17 16:20:16 +0530

    code is updated

---
+++ src/test/java/com/nacre/Jenkins_Demo_101/AppTest.java
@@ -0,0 +1,20 @@
+package com.nacre.Jenkins_Demo_101;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
