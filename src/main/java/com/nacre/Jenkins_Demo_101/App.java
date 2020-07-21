37b20f790925122d1f3130a078c42e34eec6e374
ushajagadambe <jagadambeusha@gmail.com>
2020-07-21 15:32:12 +0530

    code updated

--- src/main/java/com/nacre/Jenkins_Demo_101/App.java
+++ src/main/java/com/nacre/Jenkins_Demo_101/App.java
@@ -5,7 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    System.out.println("this my second jenkins job ");
+    System.out.println("this my third jenkins job ");
 
     
     }
