341fba9ec04472b791d3bc2e15843ebef608caf4
ushajagadambe <jagadambeusha@gmail.com>
2020-07-21 15:38:31 +0530

    program to calulate sum of digits of given number

---
+++ src/main/java/com/nacre/Jenkins_Demo_101/SumOfDigitOfGivenNumber.java
@@ -0,0 +1,23 @@
+package com.nacre.Jenkins_Demo_101;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number");
+	int n=sc.nextInt();
+	int r,sum = 0;
+	while(n!=0)
+	{
+		r=n%10;
+		sum=sum+r;
+		n=n/10;
+	}
+	System.out.println(sum);
+}
+
+    
+    }
+}
